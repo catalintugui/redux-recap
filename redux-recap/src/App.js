04ecfe6453f242a1a693357ed3ddb7b2c7856727
Catalin Tugui <katamaxx23@gmail.com>
2022-09-19 19:37:40 +0300

    added funny login

--- redux-recap/src/App.js
+++ redux-recap/src/App.js
@@ -1,19 +1,32 @@
 import './App.css';
 import {useSelector, useDispatch} from 'react-redux'
 import { increment, decrement } from './actions';
+import {login} from './actions'
 
 function App() {
   const score = useSelector(state => state.counter);
-  // const isLogged = useSelector(state => state.isLogged);
+  const isLogged = useSelector(state => state.isLogged);
   const dispatch = useDispatch();
+  const loggedIn = 'You are logged in';
+  const loggedOut = 'You are logged out';
+  const loginButton = 'Login';
+  const logoutButton = 'Logout';
 
   return (
     <div className="App">
-      Your score is: {score}
       <div>
-        <button onClick={() => dispatch(increment())}>PLUS +</button>
-        <button onClick={() => dispatch(decrement())}>MINUS -</button>
+        {isLogged ? <h2>{loggedIn}</h2> : <h2>{loggedOut}</h2>}
+        <button onClick={() => dispatch(login())}>{isLogged ? logoutButton : loginButton}</button>
       </div>
+      {isLogged &&
+      <div>
+        <h2>Your score is: {score}</h2>
+        <div>
+          <button onClick={() => dispatch(increment())}>PLUS +</button>
+          <button onClick={() => dispatch(decrement())}>MINUS -</button>
+        </div>
+      </div>
+      }
     </div>
   );
 }
